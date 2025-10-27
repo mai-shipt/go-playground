e7892a47f64f633cd5b470b627084be517bb8daf
mai-shipt <mai.thao@shipt.com>
2025-10-27 10:48:54 -0500

    Create newDeck init function

--- section-three/cards/deck.go
+++ section-three/cards/deck.go
@@ -5,7 +5,25 @@ import "fmt"
 // Type of 'deck' has all features of a slice of strings
 type deck []string
 
-// print() method is related to "type" deck
+// newDeck() create and return a list of playing cards (aka a deck: an array of strings)
+// Acts like a constructor, returns a value instance of 'deck'
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	// Use blank identifier '_' to ignore index
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+// print() log out the contents of a deck of cards
+// This method is related to "type" deck
 // Any variable of type "deck" have access to this "print" method
 func (d deck) print() {
 	for i, card := range d {
