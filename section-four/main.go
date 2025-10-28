7618108be25408d39921d14f4fa4e9412d73cf9f
mai-shipt <mai.thao@shipt.com>
2025-10-28 15:25:08 -0500

    Enhance person struct with embedded struct

--- section-four/main.go
+++ section-four/main.go
@@ -2,24 +2,42 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 // Define a struct
 // This is a custom type called 'person' with these fields/properties
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// Declare a struct
-	// Create a new person with these values
-	mai := person{firstName: "Mai", lastName: "Thao"}
-	fmt.Println(mai)
-
-	// Another way to declare a struct
-	var alex person
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex) // Print all different field names and values
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Printf("%+v", alex)
+	mai := person{
+		firstName: "Mai",
+		lastName:  "Thao",
+		contact: contactInfo{
+			email:   "maithao@demo.com",
+			zipCode: 12345,
+		},
+	}
+	fmt.Printf("%+v\n", mai)
 }
+
+//func main() {
+//	// Declare a struct
+//	// Create a new person with these values
+//	mai := person{firstName: "Mai", lastName: "Thao"}
+//	fmt.Println(mai)
+//
+//	// Another way to declare a struct
+//	var alex person
+//	fmt.Println(alex)
+//	fmt.Printf("%+v", alex) // Print all different field names and values
+//	alex.firstName = "Alex"
+//	alex.lastName = "Anderson"
+//	fmt.Printf("%+v", alex)
+//}
