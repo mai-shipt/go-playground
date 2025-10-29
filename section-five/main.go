831acafbdd73aa64526935ed998e9c2e600e2d86
mai-shipt <mai.thao@shipt.com>
2025-10-29 15:52:59 -0500

    Manipulate maps with 2 other syntax

--- section-five/main.go
+++ section-five/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
+	// Syntax 1 - with var keyword, if we need to figure out what values to add later
+	var colors map[string]string
+	colors = make(map[string]string)
+
+	// Syntax 2 - with 'make' built-in fxn (exact equivalent to syntax 1)
+	colorsAlt := make(map[string]string)
+
+	// Syntax 2 - but with inlined value initialization
+	colorsAlt2 := map[string]string{
 		"red":    "#ff0000",
 		"green":  "#00ff00",
 		"blue":   "#0000ff",
@@ -11,7 +19,14 @@ func main() {
 		"purple": "#00ff00",
 	}
 
-	fmt.Println(colors)
-	fmt.Println(colors["red"])
+	colors["black"] = "000000"
+	colorsAlt["white"] = "#ffffff"
 
+	// Remove entries from a map
+	delete(colors, "black")
+	delete(colorsAlt2, "yellow")
+
+	fmt.Println(colors)
+	fmt.Println(colorsAlt)
+	fmt.Println(colorsAlt2)
 }
