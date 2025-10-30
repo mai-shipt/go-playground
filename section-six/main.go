36307e301f64c3aca9cafe954bd59f1d0b69bb41
mai-shipt <mai.thao@shipt.com>
2025-10-30 14:06:20 -0500

    Add interface and usage examples

--- section-six/main.go
+++ section-six/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// Creates a new type 'bot' interface where if you have a fxn called
+// 'getGreeting' and you return a 'string' then you are automatically
+// a member of type 'bot' (no need to use 'extend' or 'implement'
+// keywords like other languages)
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -13,18 +21,15 @@ func main() {
 	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+// Just needed one printGreeting() fxn with one implementation
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
-//func printGreeting(eb englishBot) {
-//	fmt.Println(eb.getGreeting())
-//}
-
-func (eb englishBot) getGreeting() string {
+func (englishBot) getGreeting() string {
 	return "Hi there!"
 }
 
-func (sb spanishBot) getGreeting() string {
+func (spanishBot) getGreeting() string {
 	return "Hola!"
 }
