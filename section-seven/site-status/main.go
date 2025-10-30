8e1255b312edba077a35a35764da67766f573332
mai-shipt <mai.thao@shipt.com>
2025-10-30 15:47:58 -0500

    Introduce channels for goroutines

--- section-seven/site-status/main.go
+++ section-seven/site-status/main.go
@@ -14,18 +14,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link) // Program will terminate and not wait
+		// This will execute, but only first URL is checked... need a mechanism to block and wait!
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
 // checkLink checks each URL and see if it is responding to traffic or not
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down..."
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep it's up!"
 }
