972c31acfbb068172cbf1ca475a24324414f754e
mai-shipt <mai.thao@shipt.com>
2025-10-30 15:42:52 -0500

    Init channels program to enhance

---
+++ section-seven/site-status/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+
+}
+
+// checkLink checks each URL and see if it is responding to traffic or not
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
