4da85166fc5d0537964da98abd704c001bfd2663
mai-shipt <mai.thao@shipt.com>
2025-10-30 14:55:32 -0500

    Introduce custom implementation of Writer interface

--- section-six/http/main.go
+++ section-six/http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	// Go and make an HTTP GET request and handle response and error
 	resp, err := http.Get("http://google.com")
@@ -16,12 +18,23 @@ func main() {
 		os.Exit(1)
 	}
 
+	lw := logWriter{}
+
 	// Equivalent to bottom block of code
-	io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
 
 	//// Create an empty byte slice with spaces for 99,999 elements
 	//bs := make([]byte, 99999)
 	//// Now we can read the actual doctype HTML response
 	//resp.Body.Read(bs)
 	//fmt.Println(string(bs))
+}
+
+// Custom implementation of the Writer interface: https://pkg.go.dev/io#Writer
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+
+	fmt.Println("Just wrote this many bytes:", len(bs))
+
+	return len(bs), nil
 }
